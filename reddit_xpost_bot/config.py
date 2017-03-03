030cabe55ea120c1dc4082e4e28d72658025395e
SahilAgarwal321 <sahil.agarwal94@gmail.com>
2017-03-03 12:00:13 +0530

    formatting edits to config file

--- reddit_xpost_bot/config.py
+++ reddit_xpost_bot/config.py
@@ -6,16 +6,17 @@ client_secret = "CLIENT_SECRET"
 password = ''
 user_agent = '<platform>:<app ID>:<v1.0> (by /u/username)'
 username = ''
-message = '''x-post / crosspost bot. Tells which subreddits submission has been crossposted to.'''
+message = '''x-post / crosspost bot. \n
+Tells which subreddits submission has been crossposted to.'''
 
-sleep_after_commenting = 2 
+sleep_after_commenting = 2
 # Seconds to sleep after commenting
 
-subreddit = 'try1'
+subreddit = ''
 # subreddits = ['abc','def']
 # Subreddits the bot works on
 
-phrases = ['xpost','xposted','x-post','x-posted','x post','x posted','crosspost','crossposted','cross-post','cross-posted','cross post','cross posted']
-# phrases to identify submission as a crosspost 
+phrases = ['xpost', 'xposted', 'x-post', 'x-posted', 'x post', 'x posted', 'crosspost', 'crossposted', 'cross-post', 'cross-posted', 'cross post', 'cross posted']
+# phrases to identify submission as a crosspost
 
 reply_template = '''For mobile users : xpost subreddit links -\n'''
